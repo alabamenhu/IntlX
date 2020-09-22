2cbd319922e7d4035104dc652186f92582a468f5
matéu <mateu@softastur.org>
2020-09-21 21:41:42 -0400

    Add new test values

---
+++ resources/editor/scripts/test-values/x-anon.js
@@ -0,0 +1,17 @@
+/* Test values for the X::Anon name space */
+
+test['X::Anon::Multi'] = [
+    {
+        'routine-type' : `'sub'`,
+        'multiness' : `'multi'`,
+    },
+    {
+        'routine-type' : `'regex'`,
+        'multiness' : `'proto'`,
+    },
+    {
+        'routine-type' : `'method'`,
+        'multiness' : `'multi'`,
+    }
+
+];
--- resources/editor/scripts/test-values/x-phaser.js
+++ resources/editor/scripts/test-values/x-phaser.js
@@ -2,13 +2,12 @@
 
 /* condition is arbitrary code, phaser can be PRE or POST */
 test["X::Phaser::PrePost"] = [
-        {
-            'condition' : `"say 'hello'"`,
-            'phaser' : `"PRE"`
-        },
-        {
-            'condition' : `"$*OUT.say: ':-('"`,
-            'phaser' : `"POST"`
-        }
-    ]
-};
+    {
+        'condition' : `"say 'hello'"`,
+        'phaser' : `"PRE"`
+    },
+    {
+        'condition' : `"$*OUT.say: ':-('"`,
+        'phaser' : `"POST"`
+    }
+];
